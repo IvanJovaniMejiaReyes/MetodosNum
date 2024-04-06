190688da2f68458a3ad0a59f0283583c183122aa
zeed-m11 <jovani.mm01@gmail.com>
2024-04-05 23:11:52 -0600

    NevilleCode1

--- neville_interpolacion.py
+++ neville_interpolacion.py
@@ -1,12 +1,2 @@
 import tkinter as tk
-import subprocess
 
-#Función que ejecuta mi programa de pyton
-
-def open_program(open_program):
-    try:
-        subprocess.Popen(["pyton", f"{program_name}.py"])
-    except FileExistsError:
-        messagebox.showerror("Error," f"El archivo del programa {program_name}.py no se encontro.")
-
-#Se crea la Ventana principal
